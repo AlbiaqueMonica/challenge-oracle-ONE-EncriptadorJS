0b0d7b7f2d76a28283c4ab27f8c2ed2075cbf19f
AlbiaqueMonica <monic.albiaque@gmail.com>
2022-12-27 00:10:23 -0300

    Funcionalidad Desencriptar

--- app.js
+++ app.js
@@ -26,9 +26,45 @@ function encriptar() {
      textoEncriptado = textoEncriptado.replace(/u/mg, 'ufat');
 
      document.getElementById("ocultarDoll").style.display="none";
+     document.getElementById("aviso1").style.display="none";
+     document.getElementById("aviso2").style.display="none";
+
+     document.getElementById("textoMostrar").value = textoEncriptado;
+
+     document.getElementById("textoMostrar").style.display = 'block';
+
+     document.getElementById("copiar").style.display = 'block';
+
+
+
+     
+
+}
+
+function desencriptar() {
+
+    const frase = document.getElementById("textoEncriptar").value.toLowerCase();
+    console.log(frase);
+
+    let textoEncriptado = frase.replace(/enter/mg, 'e');
+    console.log(textoEncriptado);
+     textoEncriptado = textoEncriptado.replace(/ober/mg, 'o');
+     console.log(textoEncriptado);
+     textoEncriptado = textoEncriptado.replace(/imes/mg, 'i');
+     textoEncriptado = textoEncriptado.replace(/ai/mg, 'a');
+     textoEncriptado = textoEncriptado.replace(/ufat/mg, 'u');
+
+     document.getElementById("ocultarDoll").style.display="none";
 
      document.getElementById("textoMostrar").value = textoEncriptado;
 
      
 
+}
+
+function copiarTexto(){
+
+    const textoContenido = document.querySelector("#textoMostrar")
+    textoContenido.select();
+    document.execCommand("copy");
 }
